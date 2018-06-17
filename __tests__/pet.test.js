5daee4f77b991ebf17b8b8fc56c1e336831afbc8
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-17 14:36:04 +0100

    name added and tested

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,15 @@
 const Pet = require('../src/pet.js')
 
 describe('constructor', () => {
-    it('returns an object', () => {
+  it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
+
     });
+  it('inserts the pet name', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido')
+  
+    })
+
   });
   
