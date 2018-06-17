44958a25072ce5713b3f16e1b0901ee7cb0dd635
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-06-17 15:45:01 +0100

    Walk prototype finished

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -3,13 +3,62 @@ const Pet = require('../src/pet.js')
 describe('constructor', () => {
   it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
-
     });
+
   it('inserts the pet name', () => {
       const pet = new Pet('Fido');
       expect(pet.name).toEqual('Fido')
-  
-    })
+    });
 
+
+  it('has a initial age of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+
+
+  it('has a initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+
+
+    it('has a initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
+});
+
+
+
+
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
+
+  it('increments the hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('decreases the fitness by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
   });
   
+});
+
+describe('walk', () => {
+  it('increases fitness by to a maximum of 10', () => {
+    const pet = new Pet('fido');
+    pet.fitness = 8;
+    pet.walk();
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
